dd0db3370d1875468d1c55cb8e8aa47d1f169a9c
RobertDeValentine <rjdevalentine@mail.csuchico.edu>
2020-11-25 16:48:06 -0800

    sms receival working with example on how to turn on and off relay

--- main.py
+++ main.py
@@ -1,43 +1,47 @@
 from gpiozero import *
 import time
-
-
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
-
-
 from twilio.rest import Client
+
+from signal import pause
+
+#import RPi.GPIO as GPIO
+#GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+relay = OutputDevice(20)
+relay2 = OutputDevice(21)
+relay.off()
+relay2.off()
+
+
 
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACb89f0c0970056db56a8c7c913086f222'
-auth_token = 'f347093ec0d27816142411df5361c491'
+account_sid = 'AC4718a993b32ee4a3da3a5f4b0ad8856d'
+auth_token = '43ef9aff6f02fd33c750146a7de19029'
 client = Client(account_sid, auth_token)
 
 
 
-pir = MotionSensor(4)
+#pir = MotionSensor(4)
 count=0
-relay = OutputDevice(20)
-relay2 = OutputDevice(21)
-from signal import pause
+mybool=False
+
 while True:
-    #pir.wait_for_motion()
-    t = time.localtime()
-    ct = time.strftime("%H:%M:%S" , t)
-    print(" you moved ",count,ct)
-    #message = client.messages.create(
-    #                          from_='+12183664277',
-    #                          body='sensor tripped'+str(count)+' at '+ct,
-    #                          to='+15305037021'
-    #                      )
+    print("loop start")
+    for message in client.messages.list():
+        t = time.localtime()
+        ct = time.strftime("%H:%M:%S" , t)
+        print("attempt ",count,ct)
+        print(message.from_)
+        #message = client.messages.create(
+        #                          from_='+12183664277',
+        #                          body='sensor tripped'+str(count)+' at '+ct,
+        #                          to='+15305037021'
+        #                      )
+    time.sleep(10)
 
     #print(message.sid)
-    relay.on()
-    relay2.on()
-    time.sleep(10)
-    count+=1
-    relay.off()
-    relay2.off()
-    time.sleep(10)
+    if mybool:
+        relay.on()
+        relay2.on()
+    print("loop end")
