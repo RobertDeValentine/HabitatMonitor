270919e4529e07a6bcd92e0f5b9f85ec167f3076
rob <admin@pop-os.localdomain>
2020-11-30 12:57:38 -0800

    commented current code

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+#dependencies
+#pip3 install adafruit-circuitpython-dht
+#pip3 install board
+#pip3 install gpiozero
+#pip3 install twilio
 from gpiozero import *
 import time
 import board
 import adafruit_dht
-#pip3 install adafruit-circuitpython-dht
+
 
 from twilio.rest import Client
 
 from signal import pause
 
-
+#dht sensors are unreliable and thus this function is needed
+#uses try except and recursion to sweep under the rug errors
+# and keep trying sensor until a correct reading occurs.
 def SafeDht(gpio_pin):
     dht_device = adafruit_dht.DHT22(gpio_pin,use_pulseio=False)
     try:
@@ -18,12 +25,12 @@ def SafeDht(gpio_pin):
     except RuntimeError as error:
         return SafeDht()
 
-#import RPi.GPIO as GPIO
-#GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
-sensor_pin = board.D18
-relay = OutputDevice(20)
-relay2 = OutputDevice(21)
-
+#Note this is basic on off code for relay
+#Do not exceed 10 amps @ 120V per relay if using our parts guide!
+sensor_pin = board.D18#pin for the humidity and temp sensor 
+relay = OutputDevice(20)#relay for switch connected to gpio 20
+relay2 = OutputDevice(21)#relay for switch connected to gpio 21
+#safety check switch relay devices to default off position with switch off for both outlets to go off.
 relay.off()
 relay2.off()
 
@@ -38,12 +45,12 @@ client = Client(account_sid, auth_token)
 
 
 
-#pir = MotionSensor(4)
+#debug variables
 count=0
 mybool=False
 
 while True:
-    print("loop start")
+    print("loop start")#debug
     x = '''for i in client.messages.list():
         t = time.localtime()
         ct = time.strftime("%H:%M:%S" , t)
@@ -55,12 +62,20 @@ while True:
         #                          to='+15305037021'
         #                      )
     time.sleep(10)'''#string comment to disable checking messages
-    temperature, humidity =SafeDht(sensor_pin)
+    temperature, humidity =SafeDht(sensor_pin)#call function to get temp and hum
     print(humidity," : ",temperature)
-    time.sleep(5)
+    time.sleep(5)#wait 5 seconds #debug
 
     #print(message.sid)
-    if mybool:
+    if mybool:#debug
         relay.on()
         relay2.on()
-    print("loop end")
+    print("loop end")#debug
+
+
+
+
+#todo switch on and off outlets via text
+#find way to secure account sid and token using git ignore and separate file?
+#demo environment for presentation.
+#parse messages for correct one and commands have proper responses and error messages for wrong commands.
